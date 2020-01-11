6ab7258a77d618677eb2a78863ec729d73dedfb7
Tucker Polomik <tuckerpo@buffalo.edu>
2020-01-11 01:16:05 -0500

    Fix: segfault when mime buffer doesn't contain "."

--- http/mime.c
+++ http/mime.c
@@ -15,26 +15,25 @@ char *get_mime_type (char *buf) {
             *p = tolower(*p);
             p++;
         }
-    }
-
-    if (strcmp(extension, "html") == 0 || strcmp(extension, "htm") == 0) {
-        return "text/html";
-    } else if (strcmp(extension, "js") == 0) {
-        return "application/javascript";
-    } else if (strcmp(extension, "jpeg") == 0 || strcmp(extension, "jpg") == 0) {
-        return "image/jpg";
-    } else if (strcmp(extension, "css") == 0) {
-        return "text/css";
-    } else if (strcmp(extension, "json") == 0) {
-        return "application/json";
-    } else if (strcmp(extension, "gif") == 0) {
-        return "image/gif";
-    } else if (strcmp(extension, "png") == 0) {
-        return "image/png";
-    } else if (strcmp(extension, "txt") == 0) {
-        return "text/plain";
-    } else {
-        return DEFAULT_MIME;
+        if (strcmp(extension, "html") == 0 || strcmp(extension, "htm") == 0) {
+            return "text/html";
+        } else if (strcmp(extension, "js") == 0) {
+            return "application/javascript";
+        } else if (strcmp(extension, "jpeg") == 0 || strcmp(extension, "jpg") == 0) {
+            return "image/jpg";
+        } else if (strcmp(extension, "css") == 0) {
+            return "text/css";
+        } else if (strcmp(extension, "json") == 0) {
+            return "application/json";
+        } else if (strcmp(extension, "gif") == 0) {
+            return "image/gif";
+        } else if (strcmp(extension, "png") == 0) {
+            return "image/png";
+        } else if (strcmp(extension, "txt") == 0) {
+            return "text/plain";
+        } else {
+            return DEFAULT_MIME;
+        }
     }
 
     return DEFAULT_MIME;
